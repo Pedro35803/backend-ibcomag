ceada419e3a44d44bdadc00059bd0ac74262ee9d
Pedro Júnior <pedrojuniorifpb@gmail.com>
2024-04-08 13:12:22 -0300

    Add for all origin

--- src/index.ts
+++ src/index.ts
@@ -8,12 +8,12 @@ import router from "./router";
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({ origin: "*" }));
 
 app.use("/api/v1", router);
 app.use(handleError);
 
 const port = Number(process.env.PORT) || 3000;
 app.listen(port, "0.0.0.0", () =>
-    console.log(`Aplication running in port ${port}`)
+  console.log(`Aplication running in port ${port}`)
 );
